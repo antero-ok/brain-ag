d15f6851249bcf0d4ce051c86f2f290f811ab692
Antero Nunes <antero.nns@gmail.com>
2023-03-21 20:16:49 -0300

    feat(router): App component clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import { Box, Container, Paper } from '@mui/material';
+import { FC } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
-function App() {
-  return (
-    <Container
-      sx={{
-        display: 'flex',
-        height: '100vh',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Paper
-        sx={{
-          display: 'flex',
-          width: { xs: 1, md: 800 },
-          height: { xs: 1, md: 600 },
-          p: 2,
-        }}
-        elevation={3}
-      >
-        <Box sx={{ width: '80px', border: '1px dashed red' }}>menu</Box>
-        <Box sx={{ width: 1, border: '1px dashed red' }}>content</Box>
-      </Paper>
-    </Container>
-  );
-}
+const App: FC = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
