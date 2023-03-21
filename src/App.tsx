fe53e3a1fbcadc65f03fd93e08bd3994cf649745
Antero Nunes <antero.nns@gmail.com>
2023-03-21 19:53:27 -0300

    feat(layout): adding layout barebones

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Box, Container, Paper } from '@mui/material';
+
 function App() {
-  return <div>Prod Ag</div>;
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Paper
+        sx={{
+          display: 'flex',
+          width: { xs: 1, md: 800 },
+          height: { xs: 1, md: 600 },
+          p: 2,
+        }}
+        elevation={3}
+      >
+        <Box sx={{ width: '80px', border: '1px dashed red' }}>menu</Box>
+        <Box sx={{ width: 1, border: '1px dashed red' }}>content</Box>
+      </Paper>
+    </Container>
+  );
 }
 
 export default App;
