66f3adde526af3c28e78305cd8cba3772eeb883b
Antero Nunes <antero.nns@gmail.com>
2023-03-27 18:16:08 -0300

    feat(ui): home improvements and farms data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import { FC } from 'react';
+import { Box, Container, Typography } from '@mui/material';
+import { useAppSelector } from '../global/store';
+import { formatLongNumber } from '../utils/functions';
 
-const Home = () => {
-  return <div>Home</div>;
+const Home: FC = () => {
+  const { producersList } = useAppSelector((state) => state.producer);
+  const totalFarms = producersList.length;
+  const totalFarmsArea = producersList.reduce(
+    (acc, curr) => acc + (curr.farmArea ?? 0),
+    0
+  );
+
+  return (
+    <Container
+      sx={{
+        width: 1,
+        height: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '72px',
+      }}
+    >
+      <Box>
+        <Typography align="center" fontWeight="bold" fontSize={48}>
+          {formatLongNumber(totalFarms)}
+        </Typography>
+        <Typography align="center" fontSize={28}>
+          Fazendas cadastradas
+        </Typography>
+      </Box>
+
+      <Box>
+        <Typography align="center" fontWeight="bold" fontSize={48}>
+          {formatLongNumber(totalFarmsArea)}
+        </Typography>
+        <Typography align="center" fontSize={28}>
+          Hectares de Fazendas
+        </Typography>
+      </Box>
+    </Container>
+  );
 };
 
 export default Home;
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -11,3 +11,19 @@
 export const sanitizeString = (value: string) => {
   return value?.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 };
+
+/**
+ * Returns formatted number
+ * @param {number} value Value to be formatted
+ * @returns {string} String formatted to Brazilian standards
+ *
+ * @example
+ * formatLongNumber(1000.45)
+ * // 1.000,45
+ */
+export const formatLongNumber = (value: number): string => {
+  return value.toLocaleString('pt-BR', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  });
+};
